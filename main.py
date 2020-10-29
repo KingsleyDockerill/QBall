7aabd4857d4e18c6c0a259f3ce37eb42692bdfc3
MrWhatsHisFace <Kingsmo2008@outlook.com>
2020-10-29 19:45:48 +0000

    Added run. Go to shell and do run filename. Do not include .qball extension

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import interpreter
 
 while True:
   command = input(">>> ")
+  if command[:4] == "run ":
+    command = open(f"{command[4:]}.qball").read()
   try:
     result = lexer.lexer(command).generate_tokens()
     interpreter.interpreter(result).interpret()
   except Exception as e:
-    print(e)
+    print(f"\033[91m{e}\033[00m")
